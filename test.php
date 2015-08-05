8fbd027fb915f864bffc80476e44e7b845c3990d
pavelbur <buros11223344@gmail.com>
2015-08-05 22:52:55 +0300

    uu

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link= mysqli_connect('localhost','root','','test');
-$sql= "SELECT COUNT(*) AS id FROM news";
-$ret=mysqli_query($link,$sql);
-$count=mysqli_fetch_assoc($ret);
-$count=(int) $count['id'];
-var_dump($count);
